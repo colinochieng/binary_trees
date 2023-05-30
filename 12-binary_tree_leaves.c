a7a3fcc10282faae3e8bcc3ffea4f8bd62d0bd52
colinochieng <colinokumu@gmail.com>
2023-05-30 13:12:14 -0700

    n-arr = 2

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -13,10 +13,12 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t leaf = 0;
 
-	if (!tree->left || !tree->right)
-		return (1);
 	if (!tree)
 		return (0);
+
+	if (!tree->left || !tree->right)
+		return (1);
+
 	leaf +=  binary_tree_leaves(tree->left);
 	leaf += binary_tree_leaves(tree->right);
 	return (leaf);
