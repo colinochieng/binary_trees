f0ef7cfa7ad05e532385424ba6e0d196e0a2a1cb
colinochieng <colinokumu@gmail.com>
2023-06-01 15:09:56 -0700

    n-arr = 2

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -72,7 +72,7 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 	queue[rear++] = (binary_tree_t *)tree;
 
 	if (!queue)
-		return (NULL);
+		return;
 
 	while (front < rear)
 	{
